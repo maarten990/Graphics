4451cb989c79d1679d77c156fd64177a7dfd5574
Inge Becht <inge.becht91@gmail.com>
2013-02-06 07:52:11 +0100

    fourth octant working

--- week_1/basic_midpoint/mla.c
+++ week_1/basic_midpoint/mla.c
@@ -139,7 +139,7 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
 
 
     // octant 8 Works x0 < x1, y1 < y0
-    else if(slope >= -1 && slope < 0)
+    else if(slope >= -1 && slope < 0 && x1 > x0)
     {
 
     double d =  ((y0 - y1) * (x0 + 1) + (x1 - x0) * (y0 - 0.5) + x0 * y1 - x1 * y0);
@@ -162,14 +162,30 @@ void mla(SDL_Surface *s, int x0, int y0, int x1, int y1, Uint32 colour) {
         }
         PutPixel(s,x1,y1, colour);
     }
-    // Octant 7
-    else if(slope < -1)
+    // Octant 4 works: x1 < x0, y0 < y1
+    else if(slope >= -1 && slope < 0 && x1 < x0)
     {
 
+    double d =  ((y0 - y1) * (x0 - 1) + (x1 - x0) * (y0 + 0.5) + x0 * y1 - x1 * y0);
 
+        for(x = x0 ; x > x1; x--)
+        {
 
+            PutPixel(s,x,y,colour);
+            if( d < 0)
+            {
+                y = y + 1;
+                d = d + (x0 - x1) + (y0 - y1);
 
-        
+            }
+            else
+            {
+                d = d + (y0 - y1);
+            }
+
+        }
+        PutPixel(s,x1,y1, colour);
+
     }
 
 
