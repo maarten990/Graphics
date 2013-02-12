71698ee0217b2f6e21daf331c4ddb5ac5ed3babe
Inge Becht <inge@crunchbang-vm.(none)>
2013-02-12 14:42:06 +0100

    created scale, translate and rotation matrix function

--- week_2/transformations_and_gimbal_lock/transformations.c
+++ week_2/transformations_and_gimbal_lock/transformations.c
@@ -27,9 +27,9 @@ void myScalef(GLfloat x, GLfloat y, GLfloat z)
 {
     GLfloat M[16] =
     {
-        1.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
+        x, 0.0, 0.0, 0.0,
+        0.0, y, 0.0, 0.0,
+        0.0, 0.0, z, 0.0,
         0.0, 0.0, 0.0, 1.0
     };
 
@@ -44,10 +44,20 @@ void myTranslatef(GLfloat x, GLfloat y, GLfloat z)
         1.0, 0.0, 0.0, 0.0,
         0.0, 1.0, 0.0, 0.0,
         0.0, 0.0, 1.0, 0.0,
-        0.0, 0.0, 0.0, 1.0
+        x, y, z, 1.0
     };
 
     glMultMatrixf(M);
+}
+
+
+// Returns cross product of two input vectors to last vector
+void crossProduct(GLfloat v1[], GLfloat v2[], GLfloat v3[])
+{
+
+    v3[0] = v1[1] * v2[2] - v1[2] * v2[1];
+    v3[1] = v1[2] * v2[0] - v1[0] * v2[2];
+    v3[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
 
 void myRotatef(GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
@@ -57,16 +67,49 @@ void myRotatef(GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
     //
     // 1. Create the orthonormal basis
     //
-
+    
     // Store the incoming rotation axis in w and normalize w
+    GLfloat length_a;
+    length_a = sqrt(x * x + y * y + z * z );
+    w[0] = x / length_a;
+    w[1] = y / length_a;
+    w[2] = z / length_a;
+
 
     // Compute the value of t, based on w
+    if(w[0] <= w[1] && w[0] <= w[2])
+    {
+        t[0] = 1;
+        t[1] = w[1];
+        t[2] = w[2];
+    }
+    if(w[1] <= w[2] && w[1] <= w[0])
+    {
+        t[1] = 1;
+        t[0] = w[0];
+        t[2] = w[2];
+    }
+    if(w[2] <= w[1] && w[2] <= w[0])
+    {
+        t[2] = 1;
+        t[1] = w[1];
+        t[0] = w[0];
+    }
+
 
     // Compute u = t x w
+    crossProduct(t, w, u);
+  
+
 
     // Normalize u
+    GLfloat length_u =sqrt(u[0] * u[0] + u[1] * u[1] + u[2] * u[2]);
+    u[0] = u[0] / length_u;
+    u[1] = u[1] / length_u;
+    u[2] = u[2] / length_u;
 
     // Compute v = w x u
+    crossProduct(w, u, v);
 
     // At this point u, v and w should form an orthonormal basis.
     // If your routine does not seem to work correctly it might be
@@ -78,11 +121,12 @@ void myRotatef(GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
 
     // Specify matrix A
 
+    angle = angle * (M_PI/180);
     GLfloat A[16] =
     {
-        1.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
+        u[0], u[1], u[2], 0.0,
+        v[0], v[1], v[2], 0.0,
+        w[0], w[1], w[2], 0.0,
         0.0, 0.0, 0.0, 1.0
     };
 
@@ -92,8 +136,8 @@ void myRotatef(GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
 
     GLfloat B[16] =
     {
-        1.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
+        cos(angle), sin(angle), 0.0, 0.0,
+        -sin(angle), cos(angle), 0.0, 0.0,
         0.0, 0.0, 1.0, 0.0,
         0.0, 0.0, 0.0, 1.0
     };
@@ -102,16 +146,16 @@ void myRotatef(GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
 
     GLfloat C[16] =
     {
-        1.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
+        u[0], v[0], w[0], 0.0,
+        u[1], v[1], w[1], 0.0,
+        u[2], v[2], w[2], 0.0,
         0.0, 0.0, 0.0, 1.0
     };
 
     //
     // 3. Apply the matrices to get the combined rotation
     //
-
+ 
     glMultMatrixf(A);
     glMultMatrixf(B);
     glMultMatrixf(C);
