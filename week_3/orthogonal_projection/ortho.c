3e953f5c6bc600ac21da191803a1de857e2dcd56
Maarten de Jonge <mdejonge1990@gmail.com>
2013-02-18 20:28:25 +0000

    first non-working version of myOrtho

--- week_3/orthogonal_projection/ortho.c
+++ week_3/orthogonal_projection/ortho.c
@@ -31,6 +31,31 @@ void myOrtho(GLdouble left,
              GLdouble bottom,
              GLdouble top,
              GLdouble near,
-             GLdouble far) {
+             GLdouble far)
+{
+    // secondly, scale the rectangle into a cube
+    GLfloat M[16] =
+    {
+        0.5 * (right - left), 0, 0, 0,
+        0, 0.5 * (top - bottom), 0, 0,
+        0, 0, 0.5 * (near - far), 0,
+        0, 0, 0, 1
+    };
 
+    glMultMatrixf(M);
+
+    // first translate to the origin
+    GLfloat T[16] =
+    {
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        // final column
+        -0.5 * (left + right),
+        -0.5 * (bottom + top),
+        -0.5 * (near + far),
+        1
+    };
+
+    glMultMatrixf(T);
 }
