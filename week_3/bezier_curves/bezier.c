5ec74eb6cb47c3a0a747722bab1f6d3ce3333a1c
Inge Becht <inge@crunchbang-vm.(none)>
2013-02-19 21:11:03 +0100

    merged maartens bezier with master

--- week_3/bezier_curves/bezier.c
+++ week_3/bezier_curves/bezier.c
@@ -18,6 +18,32 @@
 #include "bezier.h"
 #include <stdio.h>
 
+// Returns the factorial of n.
+long factorial(int n)
+{
+    if (n < 2)
+        return 1;
+
+    else {
+        long fact = 2;
+        for (int i = 3; i <=n; ++i) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+}
+
+// Returns the value of u as input to the i'th Bernstein polynomial of the n'th
+// degree
+float bernstein(int i, int n, float u)
+{
+    // calculate binomial(n, i)
+    float n_over_i = (float)factorial(n) / ( factorial(i) * factorial(n - i) );
+
+    return n_over_i * pow(u, i) * pow(1 - u, n - i);
+}
+
 /* Given a Bezier curve defined by the 'num_points' control points
  * in 'p' compute the position of the point on the curve for parameter
  * value 'u'.
@@ -31,6 +57,19 @@ evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, flo
 {
     *x = 0.0;
     *y = 0.0;
+
+    // the result is a summation of Bernstein polynomials over each control
+    // point
+    for (int i = 0; i < num_points; i++) {
+        if(num_points == 2)
+        {
+            printf("px = %f, pz = %f\n", p[i].x, p[i].y);
+            printf("u = %f\n", u);
+            printf("i = %f\n", i);
+        }
+        *x += bernstein(i, num_points-1, u) * p[i].x;
+        *y += bernstein(i, num_points-1, u) * p[i].y;
+    }
 }
 
 /* Draw a Bezier curve defined by the control points in p[], which
@@ -57,6 +96,20 @@ evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, flo
 void
 draw_bezier_curve(int num_segments, control_point p[], int num_points)
 {
+    float stepsize = (float)1 / num_segments;
+    float x = 0,
+          y = 0;
+
+    glBegin(GL_LINE_STRIP);
+
+    for (float u = 0; u <= 1.0; u += stepsize) {
+        evaluate_bezier_curve(&x, &y, p, num_points, u);
+        if(num_points == 2 && num_segments == 2)
+           printf("x = %f, z = %f\n", x, y);
+        glVertex2f(x, y);
+    }
+
+    glEnd();
 }
 
 /* Find the intersection of a cubic Bezier curve with the line X=x.
