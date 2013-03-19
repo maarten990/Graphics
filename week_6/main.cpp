70be84848bef2ced0138db489de9e5d1e03ebfb0
Inge Becht <inge@crunchbang-vm.(none)>
2013-03-19 23:20:55 +0100

    succesfully draws rigid shapes

--- week_6/main.cpp
+++ week_6/main.cpp
@@ -29,7 +29,6 @@ unsigned int num_levels;
 level_t *levels;
 
 // Types needed for creating a body
-b2Body* bodies[20];
 b2Body* ball;
 b2BodyDef bodyDef;
 // Defining world; only needs to be done once
@@ -40,6 +39,8 @@ b2World world(gravity);
 float32 timeStep = 1.0f / 60.0f;
 int32 velocityIterations = 6;
 int32 positionIterations = 2;
+
+level_t l;
 
 /*
  * Load a given world, i.e. read the world from the `levels' data structure and
@@ -56,13 +57,40 @@ void load_world(unsigned int level)
     }
     // Create a Box2D world and populate it with all bodies for this level
     // (including the ball).
-    level_t l = levels[level];
+    l = levels[3];
 
     // Create ball and put at the beginning position
     bodyDef.type = b2_dynamicBody;
     bodyDef.position.Set(l.start.x, l.start.y);
     ball = world.CreateBody(&bodyDef);
-    
+    b2CircleShape ballShape;
+    ballShape.m_radius = 0.2;
+    ball->CreateFixture(&ballShape, 0.0f);
+
+    poly_t p;
+
+    // The body and shape for the general poly builds
+    b2PolygonShape pol_shape;
+    b2BodyDef pol_body_def;
+    b2Body*  pol_body;
+
+    // Loop through all polygons
+    for(unsigned int i = 0; i < l.num_polygons; i ++)
+    {
+        p = l.polygons[i];
+        b2Vec2 *vertices = new b2Vec2[p.num_verts];
+        // Set all vetices over into b2Vec2 vertices
+        for(unsigned int j = 0; j < p.num_verts; j++ )
+        {
+            vertices[j].Set(p.verts[j].x, p.verts[j].y);
+        }
+
+        // Add body_def to the worl object
+        pol_body  = world.CreateBody(&pol_body_def);
+        // Create the fixture and set vertices
+        pol_shape.Set(vertices, p.num_verts);
+        pol_body->CreateFixture(&pol_shape, 0.0f);
+    }
 }
 
 
@@ -82,18 +110,68 @@ void draw(void)
     glColor3f(0, 0, 0);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Draw circles
-    glColor3f(1, 0, 0);
-    b2Vec2 position = ball->GetPosition();
-    
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(position.x, position.y);
-    glVertex2f(position.x, position.y + 0.2);
-    glVertex2f(position.x + 0.2, position.y + 0.2);
-    glVertex2f(position.x + 0.2, position.y - 0.2);
-    glVertex2f(position.x - 0.2, position.y - 0.2);
-    glEnd();
-    
+    b2Body* body_list = world.GetBodyList();
+    b2Body* body = body_list;;
+    b2Fixture* types;
+    b2Vec2 position;
+    b2Shape* shape;
+
+    while(true)
+    {    
+        types = body->GetFixtureList();
+        position = body->GetPosition();
+        
+        while(true)
+        {
+
+            shape = types->GetShape();
+            /*
+            // Draw circle
+            if(shape)// == b2Shape::e_circle)
+            {
+                glColor3f(1, 0, 0);
+                glBegin(GL_TRIANGLE_FAN);
+                glVertex2f(position.x, position.y);
+                glVertex2f(position.x, position.y + shape->m_radius);
+                glVertex2f(position.x + shape->m_radius, position.y + shape->m_radius);
+                glVertex2f(position.x + shape->m_radius, position.y - shape->m_radius);
+                glVertex2f(position.x - shape->m_radius, position.y - shape->m_radius);
+                glEnd();
+            }
+            // Draw polygon
+            else
+            {*/
+                b2PolygonShape* pol_shape = static_cast<b2PolygonShape*>(types->GetShape());
+
+                glColor3f(0, 1, 0);
+                glBegin(GL_TRIANGLE_FAN);
+                // Draw all the vertices until there are no new ones
+                for(int i = 0; i < pol_shape->GetVertexCount(); i++)
+                {
+                    b2Vec2 vpos = pol_shape->GetVertex(i);
+                    glVertex2f(vpos.x, vpos.y);
+                }
+                glEnd();
+
+            //}
+
+            // Break if there are no more fixtures
+            types = types->GetNext();
+            if(types == NULL)
+            {
+                break;
+            }
+        }
+        
+        body = body->GetNext();
+        if(body == NULL)
+        {
+            break;
+        }
+
+    }
+
+     
     // Draw all the other elements
 
     // Do any logic and drawing here.
