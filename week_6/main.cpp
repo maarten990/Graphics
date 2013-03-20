e1aa810c6cb0fc5661b677b21ed4ee3cd16211b5
Inge Becht <inge@crunchbang-vm.(none)>
2013-03-20 11:44:10 +0100

    added not working area calculator and swapper

--- week_6/main.cpp
+++ week_6/main.cpp
@@ -28,9 +28,6 @@ int frame_count;
 unsigned int num_levels;
 level_t *levels;
 
-// Types needed for creating a body
-b2Body* ball;
-b2BodyDef bodyDef;
 // Defining world; only needs to be done once
 b2Vec2 gravity(0.0f, -3.0f);
 b2World world(gravity);
@@ -40,6 +37,10 @@ float32 timeStep = 1.0f / 60.0f;
 int32 velocityIterations = 6;
 int32 positionIterations = 2;
 
+
+// Array that keeps track of the user created vertices
+b2Vec2 *vertices_new = new b2Vec2[4];
+int number;
 
 /*
  * Load a given world, i.e. read the world from the `levels' data structure and
@@ -56,9 +57,11 @@ void load_world(unsigned int level)
     }
     // Create a Box2D world and populate it with all bodies for this level
     // (including the ball).
-    level_t l = levels[3];
+    level_t l = levels[2];
 
     // Create ball and put at the beginning position
+    b2Body* ball;
+    b2BodyDef bodyDef;
     bodyDef.type = b2_dynamicBody;
     bodyDef.position.Set(l.start.x, l.start.y);
     ball = world.CreateBody(&bodyDef);
@@ -66,9 +69,9 @@ void load_world(unsigned int level)
     ballShape.m_radius = 0.2;
     ball->CreateFixture(&ballShape, 0.0f);
 
-    poly_t p;
 
     // The body and shape for the general poly builds
+    poly_t p;
     b2PolygonShape pol_shape;
     b2BodyDef pol_body_def;
     b2Body*  pol_body;
@@ -79,7 +82,7 @@ void load_world(unsigned int level)
         p = l.polygons[i];
         b2Vec2 *vertices = new b2Vec2[p.num_verts];
 
-        // Set all vetices over into b2Vec2 vertices
+        // Set all vertices over into b2Vec2 vertices
         for(unsigned int j = 0; j < p.num_verts; j++ )
         {
             vertices[j].Set(p.verts[j].x, p.verts[j].y);
@@ -87,7 +90,7 @@ void load_world(unsigned int level)
 
         // Add body_def to the world object
         pol_body  = world.CreateBody(&pol_body_def);
-        
+
         // Create the fixture and set vertices
         pol_shape.Set(vertices, p.num_verts);
         pol_body->CreateFixture(&pol_shape, 0.0f);
@@ -104,47 +107,55 @@ void draw(void)
     int time = glutGet(GLUT_ELAPSED_TIME);
     int frametime = time - last_time;
     frame_count++;
-  
+
     world.Step(timeStep, velocityIterations, positionIterations);
 
     // Clear the buffer
     glColor3f(0, 0, 0);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    b2Body* body_list = world.GetBodyList();
-    b2Body* body = body_list;;
+    // Get a list of all the bodies
+    b2Body* body = world.GetBodyList();
+
+    // Some predeclared variables for the loop
     b2Fixture* types;
     b2Vec2 position;
-    b2Shape* shape;
 
+    // While there are still new bodies
     while(true)
     {    
         types = body->GetFixtureList();
         position = body->GetPosition();
-        
+
         while(true)
         {
-            
+
             // Draw circle
             if(types->GetType() == b2Shape::e_circle)
             {
+                // Cast to subclass shape
                 b2CircleShape* bal_shape = static_cast<b2CircleShape*>(types->GetShape());
                 glColor3f(1, 0, 0);
+
+                //draw vertices
                 glBegin(GL_TRIANGLE_FAN);
                 glVertex2f(position.x, position.y);
                 glVertex2f(position.x, position.y + bal_shape->m_radius);
-                glVertex2f(position.x + 0.2, position.y + 0.2);
-                glVertex2f(position.x + 0.2, position.y - 0.2);
-                glVertex2f(position.x - 0.2, position.y - 0.2);
+                glVertex2f(position.x + bal_shape->m_radius, position.y + bal_shape->m_radius);
+                glVertex2f(position.x + bal_shape->m_radius, position.y - bal_shape->m_radius);
+                glVertex2f(position.x - bal_shape->m_radius , position.y - bal_shape->m_radius);
                 glEnd();
             }
             // Draw polygon
             else
             {
+                // Cast to subclass shape
                 b2PolygonShape* pol_shape = static_cast<b2PolygonShape*>(types->GetShape());
 
+                //printf("position x y: %f %f\n", position.x, position.y);
                 glColor3f(0, 1, 0);
                 glBegin(GL_TRIANGLE_FAN);
+
                 // Draw all the vertices until there are no new ones
                 for(int i = 0; i < pol_shape->GetVertexCount(); i++)
                 {
@@ -162,20 +173,17 @@ void draw(void)
                 break;
             }
         }
-        
+
+        // Get the next body in the linked list
         body = body->GetNext();
+
+        // Break if there are no new bodies
         if(body == NULL)
         {
             break;
         }
 
     }
-
-     
-    // Draw all the other elements
-
-    // Do any logic and drawing here.
-    //
 
 
     // Show rendered frame
@@ -186,8 +194,8 @@ void draw(void)
     {
         char window_title[128];
         snprintf(window_title, 128,
-                "Box2D: %f fps, level %d/%d",
-                frame_count / (frametime / 1000.f), -1, num_levels);
+                 "Box2D: %f fps, level %d/%d",
+                 frame_count / (frametime / 1000.f), -1, num_levels);
         glutSetWindowTitle(window_title);
         last_time = time;
         frame_count = 0;
@@ -216,10 +224,68 @@ void key_pressed(unsigned char key, int x, int y)
         case 'q':
             exit(0);
             break;
-        // Add any keys you want to use, either for debugging or gameplay.
+
+            // Add any keys you want to use, either for debugging or gameplay.
         default:
             break;
     }
+}
+
+
+// Calculates the area of a given polygon
+double calculate_area()
+{
+    double area = 0;
+    for(int i = 0 ; i < 3; i ++)
+    {
+        area += (vertices_new[i].x * vertices_new[i+1].y) - 
+            (vertices_new[i+1].x * vertices_new[i].y);
+
+    }
+
+    return 0.5 * area;
+}
+// Reverse the order of the elements
+b2Vec2* swap_elements(b2Vec2* vertices, int num_vertices)
+{
+    b2Vec2 *temp = new b2Vec2[num_vertices];
+    // Swap last elements to first elements
+    for(int i = 0; i < num_vertices; i++)
+    {
+        temp[i].x = vertices[num_vertices - i].x;
+        temp[i].y = vertices[num_vertices - i].y;
+    }
+    return temp;
+}
+
+
+void create_dynamic_object()
+{
+
+    b2BodyDef bodyDef;
+
+    bodyDef.type = b2_dynamicBody;
+
+    bodyDef.position.Set(0.0f, 4.0f);
+    b2Body* body = world.CreateBody(&bodyDef);
+
+    b2PolygonShape polyShape;
+
+    printf(" calculate area: %f", calculate_area());
+    //if(calculate_area() < 0){
+     //   vertices_new = swap_elements(vertices_new, 4);
+    //};
+    polyShape.Set(vertices_new, 4);
+
+    b2FixtureDef fixtureDef;
+
+    fixtureDef.shape = &polyShape;
+
+    fixtureDef.density = 1.0f;
+
+    fixtureDef.friction = 0.3f;
+
+    body->CreateFixture(&fixtureDef);
 }
 
 /*
@@ -227,7 +293,22 @@ void key_pressed(unsigned char key, int x, int y)
  */
 void mouse_clicked(int button, int state, int x, int y)
 {
+    if(state == GLUT_DOWN)
+    {
 
+        printf("Mouse clicked\n");
+        printf("x, y: %f, %f\n", x / 100.0, y / 100.0);
+        vertices_new[number].Set(x / 100.0, (reso_y - y) /100.0);
+        number += 1;
+
+        if(number == 4)
+        {
+            // Reset the number
+            number = 0;
+
+            create_dynamic_object();
+        }
+    }
 }
 
 /*
